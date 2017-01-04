0175c762136723953ffaec9e6ddc33231549eb96
tulliopizzuti <tullio.pizz@gmail.com>
2017-01-04 11:35:11 +0100

    Creazione javabean

---
+++ GMAsfaltiTuozzo/src/it/asflati/javabean/ProdottoBean.java
@@ -0,0 +1,37 @@
+package it.asflati.javabean;
+
+public class ProdottoBean {
+    private String id;
+    private String descrizione;
+    private String misura;
+
+    public String getId() {
+        return this.id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getDescrizione() {
+        return this.descrizione;
+    }
+
+    public void setDescrizione(String descrizione) {
+        this.descrizione = descrizione;
+    }
+
+    public String getMisura() {
+        return this.misura;
+    }
+
+    public void setMisura(String misura) {
+        this.misura = misura;
+    }
+
+    public ProdottoBean(String id, String descrizione, String misura) {
+        this.id = id;
+        this.descrizione = descrizione;
+        this.misura = misura;
+    }
+}
